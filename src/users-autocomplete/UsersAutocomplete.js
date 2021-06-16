7c1a1697cec9a47bef74b376bfb4cf5fa97ff08d
tomaslopez <tomy.lopez1999@gmail.com>
2021-06-16 01:54:40 -0300

    User profile and search

---
+++ src/users-autocomplete/UsersAutocomplete.js
@@ -0,0 +1,47 @@
+import React, {useEffect, useState} from 'react';
+import TextField from '@material-ui/core/TextField';
+import Autocomplete from '@material-ui/lab/Autocomplete';
+import {get} from "../utils/http";
+import "./UsersAutocomplete.css"
+import {useHistory} from "react-router";
+
+const UsersAutocomplete = () => {
+    const [users, setUsers] = useState([]);
+    const history = useHistory();
+
+    useEffect(() => {
+        get('auth')
+            .then(res => {
+                setUsers(res);
+            })
+    }, []);
+
+    const getUserOption = (option) => {
+        return (
+            <div className={"job-option-card"} onClick={() => history.push(`/main/home?userId=${option.id}`)}>
+                <span className={"job-option-primary-text"}>@{option.username}</span>
+                <span className={"job-option-secondary-text"}>{`${option.firstName} ${option.lastName} · ${option.email}`}</span>
+            </div>
+        )
+    };
+
+    const onValueChange = (reason) => {
+        if (reason === "clear") history.push("/main/home")
+    }
+
+    return (
+        <Autocomplete
+            id="combo-box-demo"
+            options={users}
+            getOptionLabel={option => `@${option.username}`}
+            renderOption={getUserOption}
+            style={{ width: 300 }}
+            onChange={(event, value, reason) => onValueChange(reason)}
+            renderInput={(params) =>
+                <TextField {...params} placeholder={"Search users"} variant="outlined"/>
+            }
+        />
+    );
+}
+
+export default UsersAutocomplete;

2fe177f1abae447430401027fc0faafdc72d1d43
tomaslopez <tomy.lopez1999@gmail.com>
2021-06-18 03:19:21 -0300

    Fix

--- src/chat/ChatBox.js
+++ src/chat/ChatBox.js
@@ -68,7 +68,7 @@ export const ChatBox = ({user}) => {
         stompClient.connect({}, function (frame) {
             console.log('Connected: ' + frame);
             setConnected(true);
-            stompClient.subscribe('http://localhost:8083/topic/messages/' + userInfo.id, (chatMessage) => {
+            stompClient.subscribe('/topic/messages/' + userInfo.id, (chatMessage) => {
                 incomingMessage(chatMessage);
             });
         });
@@ -104,7 +104,7 @@ export const ChatBox = ({user}) => {
             text: message,
             timestamp: Date.now()
         }]);
-        stompClient.send(`http://localhost:8083/chat/${user.id}/${userInfo.id}`, {}, JSON.stringify({
+        stompClient.send(`/chat/${user.id}/${userInfo.id}`, {}, JSON.stringify({
             'sender': userInfo.id,
             'content': message,
             'time': Date.now(),
